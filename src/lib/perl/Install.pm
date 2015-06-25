17f1a82c9f10067f60d5a68e62d2042b5a3a6fc4
athenasystems <coders@athena.systems>
2015-06-25 11:53:51 +0100

    modified:   src/www/customers/tmpl/nav.php

--- src/lib/perl/Install.pm
+++ src/lib/perl/Install.pm
@@ -3,8 +3,8 @@ package Install;
 # Web Modules Ltd. Athena Community Edition Software 2015
 # https://github.com/athenasystems/athenace The Athena Systems GitHub project
 # Author: Peter Lock - Disfit - for Web Modules Ltd.<coders@athena.systems>
-# Version: 1.1177
-# Released: Wed Jun 24 19:40:04 2015 GMT
+# Version: 1.1178
+# Released: Thu Jun 25 10:53:50 2015 GMT
 # The MIT License (MIT)
 # 
 # Copyright (c) 2015 Web Modules Ltd. UK
